909a7754dd38bac377f7d6ec8167d8ef5c92a5ca
faye <121581793+0hachi@users.noreply.github.com>
2023-06-09 00:48:00 +0200

    feat: total overhaul

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,11 @@
 # separate fisher from personal
 set fisher_path ~/.config/fish/fisher 
+
 # disable greeting message
 set fish_greeting         
+
+# add use rbin
+fish_add_path -g ~/.local/share/bin
 
 # merge keybindings
 source ~/.config/fish/functions/key_bindings.fish
--- .profile
+++
@@ -1,5 +0,0 @@
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-
-export VSCODE_PORTABLE="$XDG_DATA_HOME/vscode"
