7e17d82b87bd09a9b02f7cccf6d6421132a77419
faye <121581793+0hachi@users.noreply.github.com>
2023-06-12 06:53:20 +0200

    feat: xdg stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,26 @@
 # separate fisher from personal
-set fisher_path ~/.config/fish/fisher 
+set -Ux fisher_path ~/.config/fish/fisher 
+
+# env
+set -Ux EDITOR nvim
+set -Ux BROWSER librefox
+set -Ux TERMINAL alacritty
+
+set -Ux XDG_DATA_HOME $HOME/.local/share
+set -Ux XDG_STATE_HOME $HOME/.local/state
+set -Ux XDG_CACHE_HOME $HOME/.cache
+
+set -Ux CARGO_HOME $XDG_DATA_HOME/cargo
+set -Ux RUSTUP_HOME $XDG_DATA_HOME/rustup
+
+set -Ux GNUPGHOME $XDG_DATA_HOME/gnupg
+set -Ux XINITRC $XDG_CONFIG_HOME/X11/xinitrc
 
 # disable greeting message
 set fish_greeting         
 
-# add use rbin
-fish_add_path -g ~/.local/share/bin
+# add user bin
+fish_add_path -g $XDG_DATA_HOME/bin
 
 # merge keybindings
 source ~/.config/fish/functions/key_bindings.fish
