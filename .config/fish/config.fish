f630c6f1473a0f3e361b0f542596233ccf12f5bf
faye <121581793+0hachi@users.noreply.github.com>
2023-06-14 13:23:15 +0200

    feat: volta

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,3 +25,5 @@ fish_add_path -g $XDG_DATA_HOME/bin
 # merge keybindings
 source ~/.config/fish/functions/key_bindings.fish
 set -g fish_key_bindings key_bindings
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
